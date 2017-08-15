b7a78a6a5d524d156fd588a94789d068ed6d0587
Jose Lezama Gonzalez <joseglego@gmail.com>
2017-08-15 19:33:26 -0400

    First visual example

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // Code goes here
 $(document).ready(function () {
+  var format = 'd.m.Y H:m';
+  var formatTime = 'H:m';
+  var formatDate = 'd.m.Y';
+
   $('#datetimepicker').datetimepicker({
-    timepicker:false,
-    format:'d.m.Y H:m',
+    timepicker: true,
+    format: format,
+    onChangeDateTime: function(dp,$input){
+      setValue($input.val());
+    }
   });
+
+  $('#datetimepicker').on('keyup', function () {
+    setValue(this.value);
+  });
+
+  function setValue(value) {
+    $('#original').html(value);
+    var fmt = new DateFormatter();
+    var myDate = fmt.parseDate(value, format);
+    if (myDate) {
+      var date = document.createElement('h3');
+      date.innerHTML = fmt.formatDate(myDate, formatDate);
+      var time = document.createElement('h4');
+      time.innerHTML = fmt.formatDate(myDate, formatTime);
+      $('#processing').text('');
+      $('#processing').append(date, time);
+    } else {
+      $('#processing').html('Not VALID');
+    }
+  }
 });
