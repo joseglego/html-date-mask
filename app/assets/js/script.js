a43a9d41f84c542395c1e8d87f58f9d3561763b9
Jose Lezama Gonzalez <joseglego@gmail.com>
2017-08-22 20:10:28 -0400

    Added toggle functionality

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   const format = 'd.m.Y H:i';
   const formatTime = 'H:i A';
   const formatDate = 'd F, Y';
+  const hideClass = 'visual-datetime-hide';
 
   function setValue(input) {
     const value = input.val();
@@ -10,7 +11,7 @@ $(document).ready(function () {
     const myDate = fmt.parseDate(value, format);
     const date = document.createElement('b');
     const time = document.createElement('span');
-    const hiddenInputId = $(input).attr('data-target');
+    const hiddenInputId = $(input).attr('data-vdt-target');
 
     if (myDate) {
       date.innerHTML = fmt.formatDate(myDate, formatDate);
@@ -26,9 +27,10 @@ $(document).ready(function () {
   $('.visual-datetime').each(function () {
     const parent = $(this).parent();
     const id = $(this).attr('id');
-    $(this).css('display', 'none');
+    $(this).addClass(hideClass);
     $(parent).append(`<div class="visual-datetime-view" data-vdt-target="${id}"></div>`);
     $(parent).append(`<a href="#" class="visual-datetime-clear" data-vdt-target="${id}">Clear</a>`);
+    $(parent).append(`<a href="#" class="visual-datetime-toggle" data-vdt-target="${id}">Toggle</a>`);
   });
 
   $('.visual-datetime-view').datetimepicker({
@@ -43,5 +45,12 @@ $(document).ready(function () {
     const inputId = $(this).attr('data-vdt-target');
     $(`#${inputId}`).val('');
     $(`.visual-datetime-view[data-vdt-target=${inputId}]`).html('');
+  });
+
+  $('.visual-datetime-toggle').on('click', function (event) {
+    event.preventDefault();
+    const inputId = $(this).attr('data-vdt-target');
+    $(`#${inputId}`).toggleClass(hideClass);
+    $(`.visual-datetime-view[data-vdt-target=${inputId}]`).toggleClass(hideClass);
   });
 });
